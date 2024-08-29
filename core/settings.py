58795baf6a86d0ef29bcb2f4a8503f4250928954
Indranil <indranilchakraborty4sc@gmail.com>
2024-08-29 19:17:37 +0000

    docker setup

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^-0&_x70-d(v=7@ijz2ztx%m5ya6nt8@w-z&i+r**1t^s%#mga
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
