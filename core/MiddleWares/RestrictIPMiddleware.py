c4dfaff8023536d4072e8840bd096fd10beaf636
Sayantan Basu <sayantanbasu@live.in>
2024-10-07 03:29:19 +0530

    chagnes to middleware

--- core/MiddleWares/RestrictIPMiddleware.py
+++ core/MiddleWares/RestrictIPMiddleware.py
@@ -5,9 +5,6 @@ ALLOWED_IPS_Health_check = ["127.0.0.1"]  # Add more IPs if necessary
 
 ALLOWED_IPS_swagger = ["127.0.0.1"]
 
-ALLOWED_ORIGIN_REGEXES = [
-    r"^https?://.*\.ourkolkata\.in$",
-]
 
 class RestrictIPMiddleware:
     def __init__(self, get_response):
@@ -15,14 +12,12 @@ class RestrictIPMiddleware:
 
     def __call__(self, request):
         ip = request.META.get("REMOTE_ADDR")
-        origin = request.META.get("HTTP_ORIGIN")
-        print(origin)
+        print(ip)
 
-        if origin:
-            origin_allowed = any(re.match(pattern, origin) for pattern in ALLOWED_ORIGIN_REGEXES)
+        
         
         if request.path == "/service/health":
-            if ip not in ALLOWED_IPS_Health_check and not origin_allowed:
+            if ip not in ALLOWED_IPS_Health_check:
                 return HttpResponseForbidden("Access Denied")
             
         if request.path == "/swagger/":
