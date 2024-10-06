298eb821bb58342928ba25034ebb09d2adf4e602
Sayantan Basu <sayantanbasu@live.in>
2024-10-07 03:33:02 +0530

    grabbing IP even if req is being sent by proxy

--- core/MiddleWares/RestrictIPMiddleware.py
+++ core/MiddleWares/RestrictIPMiddleware.py
@@ -11,8 +11,12 @@ class RestrictIPMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        ip = request.META.get("REMOTE_ADDR")
-        print(ip)
+        x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+        print(x_forwarded_for)
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(",")[0].strip()
+        else:
+            ip = request.META.get("REMOTE_ADDR")
 
         
         
