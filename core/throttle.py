d552c67bc561bcc040483bf378cd6164ef2489a4
Sayantan Basu <sayantanbasu@live.in>
2024-10-12 00:03:55 +0530

    initial commit for node

--- core/throttle.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.throttling import SimpleRateThrottle
-
-
-class OneMinuteThrottle(SimpleRateThrottle):
-    scope = "one_minute"
-
-    def get_cache_key(self, request, view):
-        # Use the user's IP address as the cache key
-        return request.META.get("REMOTE_ADDR")
--- user/resource.py
+++
@@ -1,6 +0,0 @@
-from import_export import resources
-from . import models as user_models
-
-class UserResource(resources.ModelResource):
-    class Meta:
-        model = user_models.User
