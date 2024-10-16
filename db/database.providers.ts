7dcec97a6ec1ed5eb558744726a34377dee0930a
sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-10-16 09:00:51 +0530

    add: init typeorm

---
+++ db/database.providers.ts
@@ -0,0 +1,25 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: process.env.DJANGO_DB_USE,
+        password: process.env.DJANGO_DB_PASSWORD,
+        database: process.env.DJANGO_DB_NAME,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        // TODO fix: Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
